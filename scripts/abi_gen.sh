19767994e1ef050b24bb308609746400a0f83711
Ronak P <ronak@knackroot.com>
2022-01-13 18:24:48 +0530

    Upgraded abi_gen package for tuple (components ) support

--- scripts/abi_gen.sh
+++ scripts/abi_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yarn run abi-gen --abiGlob 'src/wyvern-ethereum/build/contracts/@(WyvernExchange|WyvernProxyRegistry|WyvernAtomicizer).json' --templates src/abi_templates --output src/abi_gen 
+yarn run abi-gen --abis 'src/wyvern-ethereum/build/contracts/@(WyvernExchange|WyvernProxyRegistry|WyvernAtomicizer).json' --template 'src/abi_templates/contract.mustache' --partials 'src/abi_templates/partials/*.mustache' --output 'src/abi_gen' 
